365c22c66cc09abcd5aac4ac1404e861ff2ac6f5
ойтишный <ccdeter228@gmail.com>
2025-11-27 17:44:14 +0500

    Добавление первых деталей

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame as p
+
+p.init()
+w, h = 800, 100
+s = p.display.set_mode(w, h)
+nm = "lox"
+p.display.set_caption(nm)
+
+clr1 = (222, 159, 91)
+
+g = True
+while g:
+    for e in p.event.get:
+        if e.type == p.QUIT:
+            g = False
+    
+    s.fill(clr1)
+
+p.quit()
