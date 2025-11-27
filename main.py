0e0103861f8044c895f290199acf1adb9ee2a681
ойтишный <ccdeter228@gmail.com>
2025-11-27 21:52:08 +0500

    Добавление травы

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import pygame as p
 
 p.init()
-w, h = 800, 100
+w, h = 800, 1000 
 s = p.display.set_mode((w, h))
-nm = "lox1111"
+nm = "lox"
 p.display.set_caption(nm)
 
-clr1 = (222, 159, 91)
+bg1 = (222, 159, 91)
+bg2 = (10, 124, 255)
 
+
+class Pole:
+    def __init__(self, w, h, x, y):
+        self.w = w
+        self.h = h
+        self.x = x
+        self.y = y
+        self.rect = p.Rect(self.x, self.y, self.w, self.h)
+        self.color = (25, 166, 0)
+    def draw(self, s):
+        p.draw.rect(s, self.color, self.rect)
+
+grass = Pole(200, h, 0, 0)
 g = True
 while g:
     for e in p.event.get():
         if e.type == p.QUIT:
             g = False
     
-    s.fill(clr1)
+    s.fill(bg2)
+    grass.draw(s)
     p.display.flip()
 
 p.quit()
